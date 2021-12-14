6dbc023e2f04fb56c453b052270a67b0267de77b
AliaksandrRauko <ravkoaal@mail.ru>
2021-12-14 14:14:23 +0300

    Initial Commit

---
+++ HomeWork_2_3_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork_2_3_1
+//
+//  Created by Aliaksandr Rauko on 14.12.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
